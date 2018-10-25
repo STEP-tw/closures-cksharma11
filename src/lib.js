afec8c2f112e3b3342e80d691d3d864e8138229a
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-10-25 13:44:38 +0530

    Tests passed for makeFiboGenerator

--- src/lib.js
+++ src/lib.js
@@ -27,7 +27,29 @@ const makeDeltaTracker = function(old){
   };
 };
 
-const makeFiboGenerator = undefined;
+const makeFiboGenerator = function(input,input_2){
+  input = (input == undefined) ? 0 : input;
+  let iteration = 0;
+  let initValue = input;
+  return function(){
+    let term_1 = input;
+    let term_2 = input_2;
+    if(input_2 == undefined){
+      term_1 = 0;
+      term_2 = (initValue > 0) ? initValue : 1;
+    }
+    let fibSeries = 0;
+    iteration++;
+    
+    for(let index = 0; index < iteration; index++){
+      fibSeries = term_1;
+      term_1 = term_2;
+      term_2 = term_1 + fibSeries;
+    }
+    return fibSeries;
+  }  
+};
+
 const makeCycler = undefined;
 const curry = undefined;
 const compose = undefined;
